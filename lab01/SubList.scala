21356f238f38d31140aec20825cc5f191cebcafd
IamTao <itamtao@gmail.com>
2021-09-29 17:29:48 +0200

    go through the lab.

--- lab01/SubList.scala
+++ lab01/SubList.scala
@@ -17,71 +17,134 @@ object SubList {
     }
   }.ensuring(_ => subList(l, l))
 
+  // def subListTail[T](l1: List[T], l2: List[T]): Unit = {
+  //   require(!l1.isEmpty && subList(l1, l2))
+
+  //   // Checking to be sure Stainless can reason about this
+  //   assert(!l2.isEmpty)
+
+  //   // Makes sure l1 is at the beggining of what's left of l2
+  //   if (subList(l1, l2.tail)) {
+  //     subListTail(l1, l2.tail)
+  //   } else {
+  //     // Works without else, but provides deeper insight what Stainless is capable of
+  //     assert(l1.head == l2.head)
+  //   }
+  // }.ensuring(_ => subList(l1.tail, l2))
+
   def subListTail[T](l1: List[T], l2: List[T]): Unit = {
     require(!l1.isEmpty && subList(l1, l2))
-
-    // Checking to be sure Stainless can reason about this
     assert(!l2.isEmpty)
 
-    // Makes sure l1 is at the beggining of what's left of l2
-    if (subList(l1, l2.tail)) {
-      subListTail(l1, l2.tail)
-    } else {
-      // Works without else, but provides deeper insight
-      //  what Stainless is capable of
-      assert(l1.head == l2.head)
+    (l1, l2) match {
+      case (Cons(x, xs), Cons(y, ys)) => {
+        if (x == y && subList(xs, ys)) {
+          assert(subList(l1.tail, l2))
+        } else {
+          assert(subList(l1, l2.tail))
+          subListTail(l1, l2.tail)
+        }
+      }
     }
   }.ensuring(_ => subList(l1.tail, l2))
+
+  // def subListTails[T](l1: List[T], l2: List[T]): Unit = {
+  //   require(!l1.isEmpty && !l2.isEmpty && l1.head == l2.head && subList(l1, l2))
+
+  //   // Makes sure l1 is at the beggining of what's left of l2
+  //   if (subList(l1, l2.tail)) {
+  //     subListTail(l1, l2.tail)
+  //   }
+  // }.ensuring(_ => subList(l1.tail, l2.tail))
 
   def subListTails[T](l1: List[T], l2: List[T]): Unit = {
     require(!l1.isEmpty && !l2.isEmpty && l1.head == l2.head && subList(l1, l2))
 
-    // Makes sure l1 is at the beggining of what's left of l2
-    if (subList(l1, l2.tail)) {
-      subListTail(l1, l2.tail)
-    }
+    subListTail(l1, l2)
   }.ensuring(_ => subList(l1.tail, l2.tail))
+
+  // def subListTrans[T](l1: List[T], l2: List[T], l3: List[T]): Unit = {
+  //   require(subList(l1, l2) && subList(l2, l3))
+
+  //   if (l1.isEmpty) {
+  //     // Check to make sure Stainless can prove for an empty list
+  //     assert(subList(l1, l3))
+  //   } else if (subList(l1, l2.tail)) {
+  //     // Makes sure l1 is at the beggining of what's left of l2
+  //     subListTail(l2, l3)
+  //     subListTrans(l1, l2.tail, l3)
+  //   } else if (subList(l2, l3.tail)) {
+  //     // Makes sure what's left of l2 is at the beggining of what's left of l3
+  //     subListTrans(l1, l2, l3.tail)
+  //   } else {
+  //     // Head of all three is the same so we can take it off
+  //     assert(l1.head == l3.head)
+  //     subListTrans(l1.tail, l2.tail, l3.tail)
+  //   }
+  // }.ensuring(_ => subList(l1, l3))
 
   def subListTrans[T](l1: List[T], l2: List[T], l3: List[T]): Unit = {
     require(subList(l1, l2) && subList(l2, l3))
 
-    if (l1.isEmpty) {
-      // Check to make sure Stainless can prove for an empty list
-      assert(subList(l1, l3))
-    } else if (subList(l1, l2.tail)) {
-      // Makes sure l1 is at the beggining of what's left of l2
-      subListTail(l2, l3)
-      subListTrans(l1, l2.tail, l3)
-    } else if (subList(l2, l3.tail)) {
-      // Makes sure what's left of l2 is at the beggining of what's left of l3
-      subListTrans(l1, l2, l3.tail)
-    } else {
-      // Head of all three is the same so we can take it off
-      assert(l1.head == l3.head)
-      subListTrans(l1.tail, l2.tail, l3.tail)
+    (l1, l2, l3) match {
+      case (Nil(), _, _) => assert(subList(l1, l3))
+      case (Cons(x, xs), Cons(y, ys), Cons(z, zs)) =>
+        (x == y && subList(xs, ys), y == z && subList(ys, zs)) match {
+          case (true, true) => {
+            subListTrans(xs, ys, zs)
+            assert(subList(l1, l3))
+          }
+          case (false, true) => {
+            subListTail(l1, l2)
+            subListTrans(l1, ys, zs)
+          }
+          case (true, false) => {
+            subListTail(l2, zs)
+            subListTrans(l1, l2, zs)
+          }
+          case (false, false) => {
+            subListTail(l2, l3)
+            subListTrans(l1, ys, zs)
+          }
+        }
     }
   }.ensuring(_ => subList(l1, l3))
+
+  // def subListLength[T](l1: List[T], l2: List[T]): Unit = {
+  //   require(subList(l1, l2))
+
+  //   if (l1.isEmpty) {
+  //     assert(l1.length <= l2.length)
+  //   } else if (subList(l1, l2.tail)) {
+  //     // Makes sure l1 is at the beggining of what's left of l2
+  //     subListLength(l1, l2.tail)
+  //   } else {
+  //     // Take head off both lists
+  //     subListLength(l1.tail, l2.tail)
+  //   }
+  // }.ensuring(_ => l1.length <= l2.length)
 
   def subListLength[T](l1: List[T], l2: List[T]): Unit = {
     require(subList(l1, l2))
 
-    if (l1.isEmpty) {
-      assert(l1.length <= l2.length)
-    } else if (subList(l1, l2.tail)) {
-      // Makes sure l1 is at the beggining of what's left of l2
-      subListLength(l1, l2.tail)
-    } else {
-      // Take head off both lists
-      subListLength(l1.tail, l2.tail)
+    (l1, l2) match {
+      case (Nil(), _) => assert(l1.length <= l2.length)
+      case (Cons(x, xs), Cons(y, ys)) if (x == y && subList(xs, ys)) =>
+        subListLength(xs, ys)
+      case (Cons(x, xs), Cons(y, ys)) => subListLength(l1, ys)
     }
   }.ensuring(_ => l1.length <= l2.length)
 
-  // def subListEqual[T](l1: List[T], l2: List[T]): Unit = {
-  //   require(subList(l1, l2) && l1.length >= l2.length)
+  def subListEqual[T](l1: List[T], l2: List[T]): Unit = {
+    require(subList(l1, l2) && l1.length >= l2.length)
 
-  // }.ensuring(_ =>
-  //   l1 == l2
-  // )
+    (l1, l2) match {
+      case (Cons(x, xs), Cons(y, ys)) if (x == y && subList(xs, ys)) =>
+        subListEqual(xs, ys)
+      case (Cons(x, xs), Cons(y, ys)) => subListEqual(l1, ys)
+      case _                          => ()
+    }
+  }.ensuring(_ => l1 == l2)
 
   // def subListAntisym[T](l1: List[T], l2: List[T]): Unit = {
   //   require(subList(l1, l2) && subList(l2, l1))
