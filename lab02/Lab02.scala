057ac25a23e51ddf47f93aae20fd0b5e89d0036c
IamTao <itamtao@gmail.com>
2021-10-06 19:25:19 +0200

    minor

--- lab02/Lab02.scala
+++ lab02/Lab02.scala
@@ -29,7 +29,8 @@ object Lab02 {
     decreases(t)
     t match {
       case Leaf() => true
-      case Node(root, left, right) => p(root) && forall(left, p) && forall(right, p)
+      case Node(root, left, right) =>
+        p(root) && forall(left, p) && forall(right, p)
     }
   }
 
@@ -49,18 +50,26 @@ object Lab02 {
     require(isOrdered((t)))
     t match {
       case Leaf() => Node(x, Leaf(), Leaf())
-      case Node(root, left, right) if (root > x) => Node(root, insert(left, x), right)
-      case Node(root, left, right) if (x > root) => Node(root, left, insert(right, x))
+      case Node(root, left, right) if (root > x) =>
+        Node(root, insert(left, x), right)
+      case Node(root, left, right) if (x > root) =>
+        Node(root, left, insert(right, x))
       case _ => t // case where root == x
     }
   }
 
-  def forallAfterInsert(t: Tree[BigInt], x: BigInt, p: BigInt => Boolean): Unit = {
+  def forallAfterInsert(
+      t: Tree[BigInt],
+      x: BigInt,
+      p: BigInt => Boolean
+  ): Unit = {
     require(isOrdered(t) && forall(t, p) && p(x))
     t match {
       case Leaf() => ()
-      case Node(root, left, right) if (root > x) => forallAfterInsert(left, x, p)
-      case Node(root, left, right) if (x > root) => forallAfterInsert(right, x, p)
+      case Node(root, left, right) if (root > x) =>
+        forallAfterInsert(left, x, p)
+      case Node(root, left, right) if (x > root) =>
+        forallAfterInsert(right, x, p)
       case _ => ()
     }
   } ensuring (_ => forall(insert(t, x), p))
@@ -68,7 +77,7 @@ object Lab02 {
   def orderedAfterInsert(t: Tree[BigInt], x: BigInt): Unit = {
     require(isOrdered(t))
     t match {
-      case Leaf() => ()
+      case Leaf()                                => ()
       case Node(root, left, right) if (root > x) =>
         // ???
         assert(isOrdered(insert(t, x)))
