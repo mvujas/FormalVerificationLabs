653163086d4ec22daac8c94a4c51d3ceeb4c3bf2
mvujas <18309829+mvujas@users.noreply.github.com>
2021-10-13 18:46:43 +0200

    No cache argument added to run.sh

--- lab03/run.sh
+++ lab03/run.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-stainless --watch --timeout=10 Lab03.scala
+stainless --watch --timeout=10 --functions=caseAnalysisSoundness Lab03.scala
+# stainless --timeout=10 --vc-cache=false Lab03.scala
