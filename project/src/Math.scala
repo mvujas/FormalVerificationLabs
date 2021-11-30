b891461b2d5116fa92e870c0ef066d93c4a4a8d8
mvujas <18309829+mvujas@users.noreply.github.com>
2021-11-30 19:22:26 +0100

    Full ordering interface provided

--- project/src/Math.scala
+++ project/src/Math.scala
@@ -9,8 +9,4 @@ object math {
   private def signumLemma1(x: Int): Unit = {
     require(x >= -2147483647 && x <= 2147483647)
   } ensuring (_ => signum(x) == -signum(-x))
-
-  private def signumLemma2(x: Int): Unit = {
-    require(x >= -2147483647 && x <= 2147483647)
-  } ensuring (_ => signum(x) * signum(-x) != 1)
 }
--- project/src/Ordering.scala
+++ project/src/Ordering.scala
@@ -1,3 +1,21 @@
+import math._
+import stainless.annotation._
+
+
+/**
+ * Interface for providing ordering between elements of type T.
+ * Source: https://infoscience.epfl.ch/record/268824
+ */
 trait Ordering[T] {
-  def compare(_1: T, _2: T): Int
+  def compare(x: T, y: T): Int
+
+  @law def inverse(x: T, y: T): Boolean =
+    signum(compare(x, y)) == -signum(compare(y, x))
+
+  @law def transitive(x: T, y: T, z: T): Boolean =
+    !(compare(x, y) > 0 && compare(y, z) > 0) || (compare(x, z) > 0)
+
+  @law
+  def consistent(x: T, y: T, z: T): Boolean =
+    !(compare(x, y) == 0) || (signum(compare(x, z)) == signum(compare(y, z)))
 }
