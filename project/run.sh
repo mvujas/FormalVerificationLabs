e77c1bb8b42d456ef1b48c7982ec856d18c6d36e
mvujas <18309829+mvujas@users.noreply.github.com>
2021-12-04 22:05:46 +0100

    Fixed bugs in run script

--- project/run.sh
+++ project/run.sh
@@ -1,2 +1,3 @@
-~/Downloads/stain/stainless --solvers=smt-z3 --watch --vc-cache=false --timeout=10 src/*.scala
+stainless --solvers=smt-z3 --watch --vc-cache=false --timeout=10 src/*.scala
+# stainless --solvers=smt-z3 --watch --vc-cache=false --timeout=3 --functions=treeDelMin src/*.scala
 # stainless --solvers=smt-z3 --watch --vc-cache=false --timeout=10 src/Ordering.scala src/Math.scala src/StainlessUtils.scala
