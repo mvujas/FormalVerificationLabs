fd553df28cff2ecf44604db88b0f40f7e63fcaa5
mvujas <18309829+mvujas@users.noreply.github.com>
2021-12-05 01:22:13 +0100

    assume added

--- project/src/StainlessUtils.scala
+++ project/src/StainlessUtils.scala
@@ -3,9 +3,8 @@ import stainless.annotation._
 
 object StainlessUtils {
   @extern
-  def assume(pred: () => Boolean): Unit = {
-    assert(pred())
-  }
+  def assume(b: Boolean): Unit = {
+  } ensuring (_ => b)
 
   def setForall[T](set: Set[T], predicate: T => Boolean): Boolean = forall {
     (el: T) => set.contains(el) ==> predicate(el)
