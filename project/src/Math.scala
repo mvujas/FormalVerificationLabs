15c2400d6656f9dc9fef1ed806ba8ac80717c50b
mvujas <18309829+mvujas@users.noreply.github.com>
2021-12-06 03:39:50 +0100

    Code cleaning and documentation

--- project/src/Math.scala
+++ project/src/Math.scala
@@ -6,6 +6,9 @@ object math {
   else
     0
 
+  /**
+   * Lemma proving that for an arbitary integer x: signum(x) = -signum(-x)
+   */
   private def signumLemma1(x: Int): Unit = {
     require(x >= -2147483647 && x <= 2147483647)
   } ensuring (_ => signum(x) == -signum(-x))
