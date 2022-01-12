fa53fcca66dc6df524d4fb324898f3d1dedcdd7f
mvujas <18309829+mvujas@users.noreply.github.com>
2022-01-12 10:54:39 +0100

    verification results added

--- project/run.sh
+++ project/run.sh
@@ -1,1 +1,3 @@
-stainless --solvers=smt-z3 --vc-cache=false --debug=verification src/*.scala
+#!/bin/sh
+# stainless --solvers=smt-z3 --vc-cache=false --debug=verification src/*.scala
+stainless --solvers=smt-z3 --vc-cache=false --debug=verification src/*.scala > verification_results.out
